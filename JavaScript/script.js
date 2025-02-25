c60d3f98b4a83b91c8aab4cb0056f354c02645ab
松田　遥 <hayutoverycute@gmail.com>
2025-02-25 21:26:18 +0900

    1st commit

---
+++ JavaScript/script.js
@@ -0,0 +1,45 @@
+document.write("<p><b><big>課題</big></b></p>");
+
+document.write(
+  "<p>1.変数の中にtrueまたは、falseが代入された時の処理を作ってください。</p>"
+);
+
+let num = 10 > 0 ? "true" : "false";
+
+console.log(num);
+
+document.write(
+  "<p>2.代入された変数の中が5より大きい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。</p>"
+);
+
+let numA = 10;
+
+let result = numA > 5 ? "true" : "false";
+
+console.log(result);
+
+document.write(
+  "<p>3.代入された変数の中が5より小さい時にtrueの処理と、5より大きい時にfalseの処理を作ってください。</p>"
+);
+
+document.write(
+  "<p>4.代入された変数の中が10以上の時にtrueの処理と、10以下の時にfalseの処理を作ってください。</p>"
+);
+
+document.write(
+  "<p>5.代入された変数の中に値が存在する場合はtrue、存在しない場合はfalseの処理にしてください。</p>"
+);
+
+let n = null;
+
+let result = n ? "true" : "false";
+
+console.log(result);
+
+if (n) {
+  console.log("true");
+  console.log(n);
+} else {
+  console.log("false");
+  console.log(n);
+}
